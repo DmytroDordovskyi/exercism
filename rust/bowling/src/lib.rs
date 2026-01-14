76371119e54869311ade0b8e0ec96b5d3a2a3152
Dmytro Dordovskyi <dordovskydmitry@gmail.com>
2026-01-14 22:28:15 +0100

    rust: bowling. another approach

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -4,102 +4,114 @@ pub enum Error {
     GameComplete,
 }
 
-pub struct BowlingGame {
-    frames: Vec<Vec<u16>>,
-    finished: bool
+enum Frame {
+    Strike,
+    Spare(u16, u16),
+    Regular(u16, u16),
+    Tenth(Vec<u16>),
 }
+
+pub struct BowlingGame {
+    completed_frames: Vec<Frame>,
+    current_frame: Vec<u16>,
+}
+
+const MAX_PINS: u16 = 10;
+const MAX_FRAMES: usize = 10;
 
 impl BowlingGame {
     pub fn new() -> Self {
-        BowlingGame { frames: vec![], finished: false }
+        BowlingGame { completed_frames: vec![], current_frame: vec![] }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        if pins > 10 {
+        if self.completed_frames.len() == MAX_FRAMES { return Err(Error::GameComplete); }
+        if pins > MAX_PINS {
             return Err(Error::NotEnoughPinsLeft);
         }
-        if self.finished {
-            return Err(Error::GameComplete);
-        }
 
-        match self.frames.len() {
-            0 => {
-                self.frames.push(vec![pins]);
-                Ok(())
-            },
-            10 => {
-                let last_frame = self.frames.last_mut().unwrap();
-                if last_frame.len() == 1 {
-                    let value = last_frame[0];
-                    if value == 10 {
-                        last_frame.push(pins);
-                        Ok(())
-                    } else if value + pins < 10 {
-                        last_frame.push(pins);
-                        self.finished = true;
-                        Ok(())
-                    } else if value + pins == 10 {
-                        last_frame.push(pins);
-                        Ok(())
-                    } else {
-                        Err(Error::NotEnoughPinsLeft)
-                    }
+        match (self.current_frame.is_empty(), self.completed_frames.len() < MAX_FRAMES - 1) {
+            (true, true) => {
+                if pins == MAX_PINS {
+                    self.completed_frames.push(Frame::Strike);
                 } else {
-                    let value1 = last_frame[0];
-                    let value2 = last_frame[1];
-                    if (value2 + pins <= 10 || value2 == 10) && value1 == 10 || (value1 + value2 == 10){
-                        last_frame.push(pins);
-                        self.finished = true;
-                        Ok(())
-                    } else {
-                        Err(Error::NotEnoughPinsLeft)
-                    }
+                    self.current_frame.push(pins);
                 }
+            },
+            (true, false) => { self.current_frame.push(pins) },
+            (false, true) => {
+                let first_roll = self.current_frame[0];
+                if first_roll + pins == MAX_PINS {
+                    self.completed_frames.push(Frame::Spare(first_roll, pins));
+                } else if first_roll + pins < MAX_PINS {
+                    self.completed_frames.push(Frame::Regular(first_roll, pins));
+                } else {
+                    return Err(Error::NotEnoughPinsLeft);
+                }
+                self.current_frame.clear();
             },
             _ => {
-                let last_frame = self.frames.last_mut().unwrap();
-                let value = last_frame[0];
-                if last_frame.len() == 2 || (last_frame.len() == 1 && value == 10) {
-                    self.frames.push(vec![pins]);
-                    Ok(())
-                } else if value + pins <= 10 {
-                    last_frame.push(pins);
-                    Ok(())
-                } else {
-                    Err(Error::NotEnoughPinsLeft)
+                let first_roll = self.current_frame[0];
+
+                if self.current_frame.len() == 2 {
+                    let second_roll = self.current_frame[1];
+                    if first_roll == MAX_PINS && second_roll != MAX_PINS && second_roll + pins > MAX_PINS {
+                        return Err(Error::NotEnoughPinsLeft);
+                    }
+                }
+
+                self.current_frame.push(pins);
+                if self.current_frame.len() == 3 || (self.current_frame.len() == 2 && first_roll != MAX_PINS && first_roll + pins != MAX_PINS) {
+                    self.completed_frames.push(Frame::Tenth(self.current_frame.clone()));
+                    self.current_frame.clear();
                 }
             }
+        }
+
+        Ok(())
+    }
+
+    fn get_roll(&self, frame_idx: usize, roll_idx: usize) -> u16 {
+        match &self.completed_frames[frame_idx] {
+            Frame::Strike => {
+                if roll_idx == 0 {
+                    10
+                } else {
+                    self.get_roll(frame_idx + 1, 0)
+                }
+            },
+            Frame::Spare(first, second) | Frame::Regular(first, second) => {
+                if roll_idx == 0 { *first } else { *second }
+            },
+            Frame::Tenth(v) => v[roll_idx],
         }
     }
 
     pub fn score(&self) -> Option<u16> {
-        if self.finished {
-            let mut total = 0;
-            for i in 0..9 {
-                let current_frame = &self.frames[i];
-                let frame_total = current_frame.iter().sum::<u16>();
+        if self.completed_frames.len() != MAX_FRAMES {
+            return None;
+        }
 
-                if current_frame.len() == 1 {
-                    let next_frame = &self.frames[i+1];
-                    if next_frame.len() == 1 {
-                        let next_next_frame = &self.frames[i+2];
-                        total += frame_total + next_frame[0] + next_next_frame[0];
-                    } else {
-                        total += frame_total  + next_frame[0] + next_frame[1]
-                    }
-                } else if frame_total == 10 {
-                    let next_frame = &self.frames[i+1];
-                    total += frame_total + next_frame[0];
-                } else {
-                    total += frame_total;
-                }
+        let mut total = 0;
+        for i in 0..9 {
+            match &self.completed_frames[i] {
+                Frame::Strike => {
+                    total += 10 + self.get_roll(i + 1, 0) + self.get_roll(i + 1, 1);
+                },
+                Frame::Spare(first, second) => {
+                    total += first + second + self.get_roll(i + 1, 0);
+                },
+                Frame::Regular(first, second) => {
+                    total += first + second;
+                },
+                Frame::Tenth(_) => unreachable!("10th frame can't be in first 9"),
             }
+        }
 
-            let final_frame = &self.frames[9];
-            let final_frame_total = final_frame.iter().sum::<u16>();
-            total += final_frame_total;
+        if let Frame::Tenth(rolls) = &self.completed_frames[9] {
+            total += rolls.iter().sum::<u16>();
+        }
 
-            Some(total)
-        } else { None }
+        Some(total)
     }
 }
